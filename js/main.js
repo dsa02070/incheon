72d0c5e4cc2fb1dc9d13684ec3d6703ef42adee8
gitname <82920762+dsa02070@users.noreply.github.com>
2021-06-17 23:13:25 +0900

    이미지 개선, 슬라이드 스크립트 교체
    1. 기존의 인천광역시 홈페이지 하단 이미지 사이즈가 보여지는 영역과 달라 눌리는 현상이 있습니다. 이를 보완하기 위해 다른 이미지를 포토샵으로 크기를 알맞게 편집하여 심미성을 향상 시켰습니다.
    
    2. 스와이퍼 슬라이드 이슈를 개선하려고 했으나 아직 탭 버튼 이동 시 자동재생이 되지 않습니다. 다시 내일 교체할 예정 입니다.

--- js/main.js
+++ js/main.js
@@ -9,17 +9,16 @@ $(function () {
     })
 
 
-    //swiper slider
+    //swiper slider01
     let swiper01 = new Swiper(".main_slider01", {
         loop: true,
         observer: true,
         observeParents: true,
         autoplay: {
             delay: 3000,
-            disableOnInteraction: true,
         },
         pagination: {
-            el: ".swiper-pagination",
+            el: ".first-pagination",
             type: "fraction",
         },
         navigation: {
@@ -28,14 +27,13 @@ $(function () {
         },
     });
 
-    //swiper slider
+    //swiper slider02
     let swiper02 = new Swiper(".main_slider02", {
         loop: true,
         observer: true,
         observeParents: true,
         autoplay: {
             delay: 3000,
-            disableOnInteraction: true,
         },
         pagination: {
             el: ".second-pagination",
@@ -47,22 +45,47 @@ $(function () {
         },
     });
 
-    //swiper btn display event
-    $('.slider_area:nth-child(2)').addClass('off');
-    $('.first_btn').click(function () {
-        $('.slider_area:nth-child(2)').addClass('off');
-        $('.slider_area:nth-child(1)').removeClass('off');
+    // swiper slider tab menu event
+    $(function () {
+        $(".slider_area").css("display", "none");
+        $(".slider_area:nth-of-type(1)").css("display", "block");
+        $(".swiper_btn").on("click", function () {
+            let thisIndex = $(this).index() + 1;
+            $(".slider_area").css("display", "none");
+            $(".slider_area:nth-of-type(" + thisIndex + ")").css("display", "block");
+        });
+    });
+
+    // .swiper_btn .first_btn event
+    $('.swiper_btn.first_btn').click(function () {
+
+        $('.first_btn .triangle').css('display', 'block');
+        $('.swiper_btn.first_btn .first-pagination').css('display', 'block');
+        $('.swiper_btn.first_btn .first-play').css('display', 'block');
+
+        $('.swiper_btn.second_btn .second-pagination').css('display', 'none');
+        $('.swiper_btn.second_btn .second-play').css('display', 'none');
+        $('.second_btn .triangle').css('display', 'none');
     })
-    $('.second_btn').click(function () {
-        $('.slider_area:nth-child(1)').addClass('off');
-        $('.slider_area:nth-child(2)').removeClass('off');
+    // .swiper_btn .second btn event
+    $('.swiper_btn.second_btn').click(function () {
+
+        $('.first_btn .triangle').css('display', 'none');
+        $('.swiper_btn.first_btn .first-pagination').css('display', 'none');
+        $('.swiper_btn.first_btn .first-play').css('display', 'none');
+
+        $('.swiper_btn.second_btn .second-pagination').css('display', 'block');
+        $('.swiper_btn.second_btn .second-play').css('display', 'block');
+        $('.second_btn .triangle').css('display', 'block');
     })
 
-    // .main_slider hover autoplay & stop
-    $('.main_slider').mouseenter(function () {
-        swiper.autoplay.stop();
+    // .slider_area hover autoplay & stop
+    $('.slider_area').mouseenter(function () {
+        swiper01.autoplay.stop();
+        swiper02.autoplay.stop();
     }).mouseleave(function () {
-        swiper.autoplay.start();
+        swiper01.autoplay.start();
+        swiper02.autoplay.start();
     })
 
     // swiper play button event
